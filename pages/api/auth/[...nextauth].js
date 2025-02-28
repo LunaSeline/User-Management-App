91ce7411ba70ef2be80808cfc97f7015f87ae3e1
Luna_Seline <130202103+LunaSeline@users.noreply.github.com>
2025-02-28 15:46:34 +0530

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import connectDB from "../../../lib/mongodb";
+import User from "../../../models/User";
+
+export default NextAuth({
+    providers: [
+        Providers.Credentials({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                await connectDB();
+                const user = await User.findOne({ email: credentials.email });
+
+                if (!user || user.password !== credentials.password) {
+                    throw new Error("Invalid credentials");
+                }
+                return { id: user._id, email: user.email };
+            },
+        }),
+    ],
+    session: {
+        jwt: true,
+    },
+    callbacks: {
+        async session({ session, user }) {
+            session.userId = user.id;
+            return session;
+        },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+});
